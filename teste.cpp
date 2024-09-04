4749582810d4744d978300762a876a1829a4c922
anaasilvano <0001073289@senaimgaluno.com.br>
2024-09-04 19:00:37 -0300

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string nome;
+    string raca;
+    string classe;
+    int idade;
+    string sexo;
+    string aparencia;
+    string classesocial;
+    string ocupacao; 
+    
+
+     int forca;
+     int destreza;
+     int constituicao;
+     int sabedoriaecarisma;
+    int inteligencia;
+
+
+     int  espadas;
+     int arcos;
+     int medicina;
+     int furtividadeluta;
+     int etiqueta;
+
+     int armas;
+     int equipamenos;
+     
+     string loalizacao;
+     string custo;
+
+     float moedasdinheiro;
+
+     float reputacao;
+     string aliados;
+     string conexoes;
+     float niveldemagia;
+
+     cout<<"Qual sera o nome do seu personagem?" << endl;
+     cin>> nome;
+
+     cout<<" Qual a classe que deseja para sua personagem?" << endl;
+     cin>> classe;
+
+     cout<<"voce vive em um mundo mistico onde existe seres sobrenaturais, qual a raca do seu personagem? (vampiro, lobo, bruxa, fadas, gnomo, mago etc)"<<endl;
+     cin>>raca;
+
+     cout<<" qual a classe social do seu personagem? (plebeu, servo, trabalhador, nobre)"
+     cin>> classesocial;
+
+     cout<<"sua idade?";
+     cin>> idade;
+
+     cout<<"qual o seu genero (Masculino, Feminino, etc)?";
+     cin>>sexo;
+     
+     cout<<"Descreva como sera sua aparencia:" <<endl;
+     cin>> aparencia;
+
+     cout<<" sua ocupacao sera?"<<endl;
+     cin>> ocupacao;
+
+     cout<<"qual o nivel de força que voce ira ter (1-100)?" <<endl;
+     cin>> forca;
+
+     cout<<"seu nivel de destreza sera (1-100)?" <<endl;
+     cin>> destreza;
+
+     cout<<" sabedoria e carismas e o que nao pode faltar em um personagem"<<endl;
+     cout<<" o quanto de sabedoria e carisma seu personagem tera (1-100)?"<<endl;
+     cin>> sabedoriaecarisma;
+
+     cout<<"o nivel de inteligencia do personagem vai ter (1-100)?"<<endl;
+     cin>> inteligencia;
+
+
+
+     cout<<"Sua historia comeca a muito tempo atraz em mundo distante, sua vila foi invadida quando voce era apenas uma crianca, porem se lembra bem de tudo e como ocorreu. Agora ja com uma idade maior voce esta em busca de vinganca contra quem invadiu sua vila, escolha armamentos e quipamentos que iram te ajudar em sua jornada. "<<endl;
+       
+    cout<<"qual tipo espada o personagem tera, para comecar sua jornada  em busca de vinganca"<<endl;
+    cin>> espadas;
+
+    cout<<"arco tambem nao pode faltar, o quao bom em arco voce sera (1-100) ?"<<endl;
+    cin>>arcos;
+
+    cout<<"o personagem tem um nivel de medicina, qual sera o nivel (1-100)?"<<endl;
+    cin>>medicina;
+
+    cout<<"seu personagem tem muitas habilidades incluindo furtividade e luta, qual nivel voce deseja de furtividade e luta para seu personagem (1-100)?"<<endl;
+    cin>> furtividadeluta;
+
+    cout<" seu personagem vive em uma era em que etiqueta e algo fudamental, escolha qual o nivel de etiqueta o personagem tera (1-100)"<<endl;
+    cin>> etiqueta;
+
+    cout<<"seu personagem carrega uma mochila com armas brancas e algumas coisas dentro (como remedio de cura, alimentos e etc), qual sera as armas brancas que ele tem?"<<endl;
+    cin>> armas;
+
+    cout<<"para vencer a batalha o personagem tem que ter equipamentos com ele (como armaduras, escudos, e roupas com tecidos como uma alta qualidade) quais equipamentos voce vai escolher para vencer a batalha e conseguir sua vinganca?"<<endl;
+    cin>>equipamenos;
+
+    cout<<" escolha qual localizacao em que voce planeja sua vinganca"<<endl;
+    cin>>loalizacao;
+
+    cout<<"qual sera a media de custo no seu planejamento de vinganca?"<<endl;
+    cin>> custo;
+
+    cout<<"qual a quantia de moedas e dinheiro que voce recebe por fazer justica?"<<endl;
+    cin>> moedasdinheiro;
+
+    cout<<" como e sua reputacao na vila?"<<endl;
+    cin>>reputacao;
+
+    cout<<"com base na sua reputacao o quanto de alidaos voce tem, para fazer justica em busca de vingnca?"<<endl;
+    cin>>aliados;
+
+    cout<<"voce tem conexoes com outas vilas?"<<endl;
+    cin>>conexoes;
+
+    cout<<"por fim, com tudo planejado voce tem uma carta  na manga, que sera magia, escolha  o seu nivel de magia?"<<endl;
+    cin>>niveldemagia; 
+
+    cout<<" com tudo feito, sua vingaca foi finalmente feita, ocorru tudo bem e voce venceu a batalha, parabens agora voce pode descansar em paz."<<endl;
+
+
+
+
+
+    
+
+
+
+    
+
+   
+    
+}    
